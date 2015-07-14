d81b4b230747910cb30f0b10f16147b9185db49f
ThaddeusANg <thaddeus.a.ng@gmail.com>
2015-07-14 08:53:35 -0400

    refactored code, started using sigimera api for getting disaster datasets

--- client/home/home.js
+++ client/home/home.js
@@ -1,9 +1,23 @@
 if (Meteor.isClient) {
+  Session.set('loadData',false);
+
+Tracker.autorun(function() {  
+console.log(Session.get('loadData'));
+  if (Session.get('loadData') ==false) {
+  console.log('inside');    
+    var crisissearch = Meteor.subscribe("crisissearch");
+    Session.set('searching', ! crisissearch.ready());
+    Session.set('loadData',true);
+  }
+});
+
   // counter starts at 0
   Session.setDefault('loc', 0);
+  Session.set
 
 function foundLocation(location) {
-  Session.set('loc','lat: '+location.coords.latitude+', lan: '+ location.coords.longitude);
+  Session.set('lat',location.coords.latitude);
+  Session.set('long',location.coords.longitude);
 }
 
 function noLocation() {
@@ -11,17 +25,44 @@ function noLocation() {
 }
 
   Template.body.helpers({
-    tasks: [
-    {"metadata":"test"}
-    ]
+    'click button': function() {
+      event.preventDefault();
+
+        console.log('called search');
+    
+    try {
+      var response = HTTP.get('http://api.sigimera.org/v1/crises?auth_token=EtcYxoBYskcMo-cVeC8k');
+      console.log("Content"+response);
+      CrisisCollection.insert(response);
+    } catch(error) {
+      console.log(error);
+    }
+
+    }
   });
 
   Template.home.helpers({
   'location': function () {
     navigator.geolocation.getCurrentPosition(foundLocation, noLocation);
-    return Session.get('loc');
-  }
+  },
+  'crisis': function () {
+      return CrisisCollection.find();
+    },
+    'lat':function(){
+      return Session.get('lat');
+    },
+    'long':function(){
+      return Session.get('long');
+    }
+
 });
+
+  Template.home.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
 
 //end client
 }
@@ -29,5 +70,31 @@ function noLocation() {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-  });
+        console.log('called search');
+    
+    try {
+      var response = HTTP.get('http://api.sigimera.org/v1/crises?auth_token=EtcYxoBYskcMo-cVeC8k');
+      console.log("Content"+response);
+      CrisisCollection.insert(response);
+    } catch(error) {
+      console.log(error);
+    }
+    /*  _.each(names, function (name) {
+        Players.insert({name: name,score: Math.floor(Random.fraction() * 10) * 5});
+          });
+  */
+    });
+
+  Meteor.publish('crisissearch', function() {
+    console.log('called search');
+    
+    try {
+      var response = HTTP.get('http://api.sigimera.org/v1/crises?auth_token=EtcYxoBYskcMo-cVeC8k');
+      console.log("Content"+response);
+      CrisisCollection.insert(response);
+    } catch(error) {
+      console.log(error);
+    }
+});
+
 }
