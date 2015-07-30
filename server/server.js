1dd56318d14c5a9b6e8eae122d9f42afdec43393
ThaddeusANg <thaddeus.a.ng@gmail.com>
2015-07-30 15:02:44 -0400

    removed some bugged features

--- server/server.js
+++ server/server.js
@@ -61,20 +61,3 @@ Meteor.publish("userData", function () {
     }
   });
 
-Accounts.onCreateUser(function(options, user) {
-  // we wait for Meteor to create the user before sending an email
-  Meteor.setTimeout(function() {
-    Accounts.sendVerificationEmail(user._id);
-  }, 2 * 1000);
-
-  return user;
-});
-
-Accounts.validateLoginAttempt(function(attempt){
-  if (attempt.user && attempt.user.emails && !attempt.user.emails[0].verified ) {
-    console.log('email not verified');
-
-    return false; // the login is aborted
-  }
-  return true;
-}); 
